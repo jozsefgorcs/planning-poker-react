4226758bc9c27000abcaf705d29c0ea6d827d9f8
jozsefgorcs <joeygeek@gmail.com>
2023-03-20 18:51:53 +0100

    Change layout css to scoped

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { Link, NavLink, Outlet } from "react-router-dom";
-import "./Layout.css";
+import styles from "./Layout.module.css";
 type LayoutProps = {};
 
 function Layout(props: LayoutProps) {
@@ -8,13 +8,17 @@ function Layout(props: LayoutProps) {
     (state: any) => state.auth.isAuthenticated
   );
   return (
-    <div className="full-div d-flex h-100 text-center text-bg-dark">
-      <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <div
+      className={`${styles["full-div"]}  d-flex h-100 text-center text-bg-dark`}
+    >
+      <div className=" d-flex w-100 h-100 p-3 mx-auto flex-column">
         <header className="mb-auto">
           <div>
             <h3 className="float-md-start mb-0">Planning poker</h3>
             {isAuth && (
-              <nav className="nav nav-masthead justify-content-center float-md-end">
+              <nav
+                className={`${styles.navigation} ${styles["nav-masthead"]} justify-content-center float-md-end`}
+              >
                 <NavLink to="/" className="nav-link fw-bold py-1 px-0 ">
                   Home
                 </NavLink>
